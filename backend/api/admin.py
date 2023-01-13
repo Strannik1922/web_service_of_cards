ff354f7dc9e4b010caff2107fbdfd2edea68eee8
Иван Шамайский <shamaiskii.ivan@gmail.com>
2023-01-13 19:50:23 +0500

    isort

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Menu, Item
+from .models import Item, Menu
 
 
 @admin.register(Menu)
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from .views import IndexPageView
 
-
 app_name = 'api'
 
 urlpatterns = [
